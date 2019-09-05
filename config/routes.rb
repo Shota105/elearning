fcb0b9e3b28ed2302d758678eb9976be96fb0065
SATO SHOTA <satoshota@MacBook-Air-SS.local>
2019-09-05 13:57:34 +0800

    Created sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :users, except: :new
+  get '/signup', to: 'users#new'
+  get '/login', to: 'users#login'
   root 'static_pages#home'
   get 'static_pages/home', to: 'static_pages#home'
   get 'static_pages/about', to: 'static_pages#about'
