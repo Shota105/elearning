9ee2b954d3dbda4eeef91a87e7348752c6e4ea1a
Shota105 <54784646+Shota105@users.noreply.github.com>
2019-09-06 10:45:17 +0800

    Authenticate (#2)
    * Created sign-in
    
    * Create sigup page & function
    
    * Create login function

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def login
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = "Succesfully registered!"
+      redirect_to login_path
+    else
+      render "new"
+    end
+    
+
+  end
+
+  def show
+    
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+end
